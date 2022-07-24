acf7c3d8503389da124ab5b281dc2e838e968efb
TessShearer <shearer.tess@gmail.com>
2022-07-24 14:49:48 -0600

    remove Nav component, add to App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Resume from './components/Resume';
-import Contact from './components/Contact';
-import Nav from './components/Nav';
+// import Portfolio from './components/Portfolio';
+// import Resume from './components/Resume';
+// import Contact from './components/Contact';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
+  const [selectedSection, setSelectedSection] = useState("About Me")
+
   return (
     <div>
-      <Nav>
-      </Nav>
+     <header className="header">
+            <h1>
+                Tess Shearer
+            </h1>
+            <nav>
+                <ul>
+                    <li className={(selectedSection)==="About Me"?"selected navLink":"navLink"} onClick={setSelectedSection("About Me")}>
+                        About Me
+                    </li>
+                    <li className={(selectedSection)==="About Me"?"selected navLink":"navLink"} onClick={setSelectedSection("About Me")}>
+                        Portfolio
+                    </li>
+                    <li className={(selectedSection)==="About Me"?"selected navLink":"navLink"} onClick={setSelectedSection("About Me")}>
+                        Resume
+                    </li>
+                    <li className={(selectedSection)==="About Me"?"selected navLink":"navLink"} onClick={setSelectedSection("About Me")}>
+                        Contact Me
+                    </li>
+                </ul>
+            </nav>
+        </header>
       <section className="App">
         <main>
-          <About></About>
-          <Portfolio></Portfolio>
-          <Resume></Resume>
-          <Contact></Contact>
+          {selectedSection === "About Me" && <About></About>}
         </main>
       </section>
       <section className="footer">

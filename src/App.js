832febade388b2e1d7637af07856b399f9989d4e
TessShearer <shearer.tess@gmail.com>
2022-08-03 18:52:56 -0600

    created Nav and Footer components, moved Nav and Footer code into them

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Resume from './components/Resume';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Nav from './components/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
-  const [selectedSection, setSelectedSection] = useState("About Me")
+  const [selectedSection, setSelectedSection] = useState("About Me");
 
   return (
     <div>
-     <header className="header">
-            <h1>
-                Tess Shearer
-            </h1>
-            <nav>
-                <ul>
-                    <li className={(selectedSection)==="About Me"?"selected navLink":"navLink"} onClick={() => setSelectedSection("About Me")}>
-                        About Me
-                    </li>
-                    <li className={(selectedSection)==="Portfolio"?"selected navLink":"navLink"} onClick={() => setSelectedSection("Portfolio")}>
-                        Portfolio
-                    </li>
-                    <li className={(selectedSection)==="Resume"?"selected navLink":"navLink"} onClick={() => setSelectedSection("Resume")}>
-                        Resume
-                    </li>
-                    <li className={(selectedSection)==="Contact"?"selected navLink":"navLink"} onClick={() => setSelectedSection("Contact")}>
-                        Contact Me
-                    </li>
-                </ul>
-            </nav>
-        </header>
+      <header className="header">
+        <h1>
+          Tess Shearer
+        </h1>
+        <Nav selectedSection={selectedSection} setSelectedSection={setSelectedSection} />
+      </header>
       <section className="App">
         <main>
           {selectedSection === "About Me" && <About></About>}
@@ -41,10 +28,8 @@ function App() {
           {selectedSection === "Contact" && <Contact></Contact>}
         </main>
       </section>
-      <section className="footer">
-<a href="https://github.com/TessShearer">GitHub</a>
-<a href="https://www.linkedin.com/in/tess-shearer-a343b2232/">LinkedIn</a>
-<a href="https://stackoverflow.com/users/18235820/tess-shearer?tab=profile">Stack Overflow</a>
+      <section>
+        <Footer />
       </section>
     </div>
   );
